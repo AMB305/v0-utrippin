3de909399f6f28d6c38a2a8b35ede7b9cb59da5d
AMB305 <94630609+AMB305@users.noreply.github.com>
2025-07-04 17:45:27 -0400

    Update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB', // Tailwind blue-600
+        accent: '#FF6200',
+        darkblue: '#002974',
+      },
+    },
+  },
   plugins: [],
 }
