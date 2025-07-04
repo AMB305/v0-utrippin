c4b034a3409747047062093e8c1aae6b1e8418ec
AMB305 <94630609+AMB305@users.noreply.github.com>
2025-07-04 13:57:27 -0400

    Update Home page content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+
 export default function Home() {
-  return <div className='p-8 text-2xl font-bold'>This is the Home.</div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold mb-4 text-[#0055A5]">Home Page</h1>
+      <p className="text-gray-600">Welcome to Utrippin. Start exploring now!</p>
+    </div>
+  )
 }
