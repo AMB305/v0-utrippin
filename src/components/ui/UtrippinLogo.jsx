2fca15ff4ef83f69f376612bae94249bc12dbbbf
AMB305 <94630609+AMB305@users.noreply.github.com>
2025-07-05 10:13:15 -0400

    Create UTrippin Logo Component

---
+++ src/components/ui/UtrippinLogo.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function UtrippinLogo() {
+  return (
+    <div className="flex space-x-2 items-center transition-transform duration-300 hover:scale-105 hover:-translate-y-1 hover:drop-shadow-lg animate-pulse-gradient">
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#0068EF] hover:bg-[#338ef7] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">U</span>
+      </div>
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#0055A5] hover:bg-[#357ac7] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">T</span>
+      </div>
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#003C8A] hover:bg-[#345eaa] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">R</span>
+      </div>
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#FF6200] hover:bg-[#ff8533] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">I</span>
+      </div>
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#FF6200] hover:bg-[#ff8533] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">P</span>
+      </div>
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#FF6200] hover:bg-[#ff8533] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">P</span>
+      </div>
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#0055A5] hover:bg-[#357ac7] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">I</span>
+      </div>
+      <div className="block w-10 h-12 md:w-12 md:h-14 lg:w-14 lg:h-16 bg-[#0068EF] hover:bg-[#338ef7] rounded-sm flex items-center justify-center transition duration-300">
+        <span className="text-white font-extrabold text-xl md:text-2xl">N</span>
+      </div>
+    </div>
+  );
+}
